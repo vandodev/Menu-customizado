f4573977efee4dd9c5fa6fe98f829c812375acae
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-26 10:15:43 -0300

    :notebook: Transformando botão em um component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -13,68 +13,78 @@ import profile from "./assets/profile.png";
 import home from "./assets/home.png";
 
 export default function App() {
+  const [currentTab, setCurrentTab] = useState("Home");
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ justifyContent: "flex-start", padding: 20 }}></View>
-      <Image
-        source={profile}
-        style={{ width: 60, height: 60, borderRadius: 10, marginTop: 8 }}
-      ></Image>
-      <Text
-        style={{
-          fontSize: 20,
-          fontWeight: "bold",
-          color: "white",
-          marginTop: 20,
-        }}
+      <View
+        style={{ justifyContent: "flex-start", padding: 20, marginTop: 10 }}
       >
-        Elaine husenberg
-      </Text>
+        <Image
+          source={profile}
+          style={{ width: 60, height: 60, borderRadius: 10, marginTop: 8 }}
+        ></Image>
 
-      <TouchableOpacity>
         <Text
           style={{
-            marginTop: 6,
+            fontSize: 20,
+            fontWeight: "bold",
             color: "white",
+            marginTop: 20,
           }}
         >
-          Ver perfil
+          Elaine husenberg
         </Text>
-      </TouchableOpacity>
-
-      <View>
-        {
-          //Tab bar Buttons...
-        }
 
         <TouchableOpacity>
-          <View
+          <Text
             style={{
-              flexDirection: "row",
-              alignItems: "center",
-              paddingVertical: 8,
-              backgroundColor: "white",
-              paddingLeft: 20,
-              paddingRight: 40,
-              borderBottomEndRadius: 8,
+              marginTop: 6,
+              color: "white",
             }}
           >
-            <Image source={home} style={{ width: 25, height: 25 }}></Image>
-            <Text
-              style={{
-                fontSize: 15,
-                fontWeight: "bold",
-                paddingLeft: 15,
-              }}
-            >
-              Home
-            </Text>
-          </View>
+            Ver perfil
+          </Text>
         </TouchableOpacity>
+
+        <View>
+          {
+            //Tab bar Buttons...
+          }
+          {TabButton()}
+        </View>
       </View>
     </SafeAreaView>
   );
 }
+
+const TabButton = () => {
+  return (
+    <TouchableOpacity>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          paddingVertical: 8,
+          backgroundColor: "white",
+          paddingLeft: 20,
+          paddingRight: 40,
+          borderBottomEndRadius: 8,
+        }}
+      >
+        <Image source={home} style={{ width: 25, height: 25 }}></Image>
+        <Text
+          style={{
+            fontSize: 15,
+            fontWeight: "bold",
+            paddingLeft: 15,
+          }}
+        >
+          Home
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
