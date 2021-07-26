1d34fad8dcda407836abfcd4a5e26fc6cb7e4ecf
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-26 09:03:00 -0300

    :notebook:Assets e area de perfil

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import profile from "./assets/profile.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ justifyContent: "flex-start", padding: 20 }}></View>
+      <Image
+        source={profile}
+        style={{ width: 60, height: 60, borderRadius: 10, marginTop: 8 }}
+      ></Image>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold",
+          color: "white",
+          marginTop: 20,
+        }}
+      >
+        Elaine husenberg
+      </Text>
+
+      <TouchableOpacity>
+        <Text
+          style={{
+            marginTop: 6,
+            color: "white",
+          }}
+        >
+          Ver perfil
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#5359D1",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
 });
