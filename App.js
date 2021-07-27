b67dea8fdac70e87e7f079b600cc21b9c01b3955
Evandro oliveira <vandoaparecido@hotmail.com>
2021-07-27 09:21:04 -0300

    :notebook: Finalizando animação

--- App.js
+++ App.js
@@ -102,65 +102,89 @@ export default function App() {
           transform: [{ scale: scaleValue }, { translateX: offsetValue }],
         }}
       >
-        <TouchableOpacity
-          onPress={() => {
-            // Do Actions Here....
-            // Scaling the view...
-            Animated.timing(scaleValue, {
-              //toValue: 0.88,
-              toValue: showMenu ? 1 : 0.88, //Restaura a view
-              duration: 300,
-              useNativeDriver: true,
-            }).start();
-
-            Animated.timing(offsetValue, {
-              // YOur Random Value...
-              toValue: showMenu ? 0 : 230,
-              duration: 300,
-              useNativeDriver: true,
-            }).start();
-
-            setShowMenu(!showMenu);
+        <Animated.View
+          style={{
+            transform: [
+              {
+                translateY: closeButtonOffset,
+              },
+            ],
           }}
         >
+          <TouchableOpacity
+            onPress={() => {
+              // Do Actions Here....
+              // Scaling the view...
+              Animated.timing(scaleValue, {
+                //toValue: 0.88,
+                toValue: showMenu ? 1 : 0.88, //Restaura a view
+                duration: 300,
+                useNativeDriver: true,
+              }).start();
+
+              Animated.timing(offsetValue, {
+                // YOur Random Value...
+                toValue: showMenu ? 0 : 230,
+                duration: 300,
+                useNativeDriver: true,
+              }).start();
+
+              Animated.timing(closeButtonOffset, {
+                // YOur Random Value...
+                toValue: !showMenu ? -30 : 0,
+                duration: 300,
+                useNativeDriver: true,
+              }).start();
+
+              setShowMenu(!showMenu);
+            }}
+          >
+            <Image
+              source={showMenu ? close : menu}
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: "black",
+                marginTop: 40,
+              }}
+            ></Image>
+          </TouchableOpacity>
+
+          <Text
+            style={{
+              fontSize: 30,
+              fontWeight: "bold",
+              color: "black",
+              paddingTop: 20,
+            }}
+          >
+            {currentTab}
+          </Text>
+
           <Image
-            source={menu}
-            style={{ width: 20, height: 20, tintColor: "black", marginTop: 40 }}
+            source={photo}
+            style={{
+              width: "100%",
+              height: 300,
+              borderRadius: 15,
+              marginTop: 25,
+            }}
           ></Image>
-        </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+              paddingTop: 15,
+              paddingBottom: 5,
+            }}
+          >
+            Jenna Ezarik
+          </Text>
 
-        <Text
-          style={{
-            fontSize: 30,
-            fontWeight: "bold",
-            color: "black",
-            paddingTop: 20,
-          }}
-        >
-          {currentTab}
-        </Text>
-
-        <Image
-          source={photo}
-          style={{
-            width: "100%",
-            height: 300,
-            borderRadius: 15,
-            marginTop: 25,
-          }}
-        ></Image>
-        <Text
-          style={{
-            fontSize: 20,
-            fontWeight: "bold",
-            paddingTop: 15,
-            paddingBottom: 5,
-          }}
-        >
-          Jenna Ezarik
-        </Text>
-
-        <Text style={{}}>Techie, YouTuber, PS Lover, Apple Sheep's Sister</Text>
+          <Text style={{}}>
+            Techie, YouTuber, PS Lover, Apple Sheep's Sister
+          </Text>
+        </Animated.View>
       </Animated.View>
     </SafeAreaView>
   );
